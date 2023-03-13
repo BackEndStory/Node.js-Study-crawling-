328c547cd073d1d8b71063805681b65d54416f59
seungryeol <opop0421@naver.com>
2023-03-13 19:40:51 +0900

    axios,cheerio를 이용한 크롤링(추가 xlsx 활용)

--- lecture/class1.js
+++ lecture/class1.js
@@ -1,21 +1,47 @@
+const cheerio = require('cheerio');
 const parse = require('csv-parse/lib/sync');            // 버전을 4~대로 해야됨
 const fs = require('fs');                               // 파일 시스템
 const csv = fs.readFileSync('csv/data.csv');           // csv파일 가져오기
+const axios = require('axios');
+// const recodes1 = parse(csv.toString('utf-8'));           // csv가 버퍼형식이므로 한글로 전환 후 파싱
+// recodes1.forEach((i,r) => { 
+//     console.log(r,i);
+// });
 
-const recodes1 = parse(csv.toString('utf-8'));           // csv가 버퍼형식이므로 한글로 전환 후 파싱
-recodes1.forEach((i,r) => { 
-    console.log(r,i);
-});
 
-
-const  xlsx = require('xlsx');
+const xlsx = require('xlsx');
 const workbook = xlsx.readFile('xlsx/data.xlsx');
 const ws = workbook.Sheets.영화목록;                     // 데이터 액셀 시트 중 영화목록 가져오기
-const recodes2 = xlsx.utils.sheet_to_json(ws);             // 시트를 제이슨 형식으로 변환
-recodes2.forEach((r,i)=>{
-    console.log(i,r.제목,r.링크);
+const recodes2 = xlsx.utils.sheet_to_json(ws,
+   // {header:'A'}
+    );             // 시트를 제이슨 형식으로 변환  {header:'A'}를 넣어줄 시 해당 A,B에 맞게 키를 가져옴
+// console.log(ws['!ref']);
+// console.log(recodes2);
+
+recodes2.forEach((r) => {
+    console.log( r.제목, r.링크);
 })
-//console.log(recodes2);
+
+const crawler = async () => {
+    //await Promise.all( recodes2.map( async (r) =>{
+    for (const [ e,r] of recodes2.entries()) {
+       
+        const response = await axios.get(r.링크);
+        if (response.status === 200) {
+            const html = response.data;
+
+            const $ = cheerio.load(html);
+
+            const text = $('.score.score_left .star_score').text();  // 태그 시 띄어쓰기도 주의!
+            console.log(r.제목, '평점', text.trim());         // 공백제거
+        }
+
+    }
+ //   }
+ //   )
+  //  );
+};
+crawler();
 
 
 
