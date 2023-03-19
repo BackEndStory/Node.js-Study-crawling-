17bf00137c54c2a21a6eb10b430ee542df1e1809
seungryeol <opop0421@naver.com>
2023-03-19 22:01:50 +0900

    화면 스크린 샷 가져오기

--- crawling-lecture/page_screen_shoot.js
+++ crawling-lecture/page_screen_shoot.js
@@ -1,26 +1,30 @@
 const parse = require('csv-parse/lib/sync');            // 바이너리 파일을 문자열로 바꿔줌(버전을 4~대로 해야됨)
-const stringify = require('csv-stringify/lib/sync'); 
+const stringify = require('csv-stringify/lib/sync');
 const fs = require('fs');                               // 파일 시스템
 const puppeteer = require('puppeteer');
-const csv = fs.readFileSync('csv/data.csv'); 
+const csv = fs.readFileSync('csv/data.csv');
 const recodes = parse(csv.toString('utf-8'));           // csv가 버퍼형식이므로 한글로 전환 후 파싱
 const axios = require('axios');
-fs.readdir('poster', (err) => {
+fs.readdir('screenshoot', (err) => {
     if (err) {
-        console.log('poster 폴더가 없어 poster 폴더를 생성합니다.');
-        fs.mkdirSync('poster');
+        console.log('screenshoot 폴더가 없어 screenshoot 폴더를 생성합니다.');
+        fs.mkdirSync('screenshoot');
     }
 });
 
 
-const get_img_crawling = async () => {
+const screenshot_crawling = async () => {
     try {
         const result_list = [];
-        const browser = await puppeteer.launch({ headless: false });    // puppeteer 시작
+        const browser = await puppeteer.launch({
+            headless: false,
+            args: ['--window-size=1920,1080']
+        });    // puppeteer 시작
         for (const [i, r] of recodes.entries()) {
             try {
 
                 const page = await browser.newPage();
+                await page.setViewport({ width: 1920, height: 1080 });
                 await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36');
                 await page.goto(r[1]);
                 const result = await page.evaluate(() => {                    // dom. document를 쓸 때 evaluate안에다가 적는다
@@ -29,23 +33,26 @@ const get_img_crawling = async () => {
                     if (scoreEl) {
                         score = score.textContent;
                     }
-                    const imgEl = document.querySelector('.poster img');
-                    let img = '';
-                    if (imgEl) {
-                        img = imgEl.src;
-                    }
-                    return { score, img };
+                    return { score };
                 });
+
                 if (result.score) {
-                    console.log(r[0], "평점", result.score.trim(), result.img.trim());
-                    result_list.push([r[0], r[1], result.score.trim(), result.img.trim()]);
+                    console.log(r[0], "평점", result.score.trim());
+                    result_list.push([r[0], r[1], result.score.trim()]);
+
                 }
-                if (result.img) {
-                    const imgResult = await axios.get(result.img.replace(/\?.*$/,''), {   // uri ?부터 끝까지 없앤다.
-                        responseType: 'arraybuffer',                                        
-                    });
-                    fs.writeFileSync(`poster/${r[0]}.jpg`, imgResult.data);
-                }
+
+                await page.screenshot({
+                    path: `screenshoot/${r[0]}.png`,           // 스크린샷 저장 경로
+                    fullPage: false,                           // 스크린샷 전체 페이지
+                    clip: {                                    // 스크린샷 사이즈 조절
+                        x:100,
+                        y:100,
+                        width:300,
+                        height:300
+                    }
+                });           // 스크린샷 하기 
+
                 await page.waitForTimeout(1000);
             } catch (e) {
                 console.error(e);
@@ -59,4 +66,4 @@ const get_img_crawling = async () => {
     }
 };
 
-get_img_crawling();
+screenshot_crawling();
