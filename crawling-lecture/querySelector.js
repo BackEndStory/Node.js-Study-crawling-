2bfdb0b7080b7490da083224d2533a55c868cfce
seungryeol <opop0421@naver.com>
2023-03-19 22:24:21 +0900

    태그 선택자1

---
+++ crawling-lecture/querySelector.js
@@ -0,0 +1,14 @@
+/*
+document.querySelector(); 하나만 <=> $
+document.queryAll(); 모두 <=> $$
+
+ex) await page.$();
+    await page.$$();
+
+# : 태그의 아이디                     $('div#container')
+. : 태그의 클래스(여러개 가능)            
+
+$('div img');
+div 태그를 펼쳤을 때 div 안에 있는 img를 선택
+
+*/
