861da6e6dff23fe712ecc6ed2efebe4a985b5c05
seungryeol <opop0421@naver.com>
2023-03-20 15:56:46 +0900

    태그 선택자2

--- crawling-lecture/querySelector.js
+++ crawling-lecture/querySelector.js
@@ -9,6 +9,22 @@ ex) await page.$();
 . : 태그의 클래스(여러개 가능)            
 
 $('div img');
-div 태그를 펼쳤을 때 div 안에 있는 img를 선택
+div 태그를 펼쳤을 때 div 안에 있는 img를 선택 ( 부모 자손 )
 
 */
+
+/*
+
+$$('div > a > img')
+>는
+div 태그 아래에 a 태그가 있다 (부모 자식)
+
+$('.score.score_left ')
+
+. 앞에 div를 생략할 수 있다. score 과 score_left는 클래스가 두 개 일 수 있다는 것인데 .으로 연결하여 사용 가능한다. 
+
+$('').textContent   -> 태그 안에 내용 도출
+$('img[width=26]').innerHTML     -> 이미지 태그 중 width가 26인 것을 찾아라!
+
+*/
+
