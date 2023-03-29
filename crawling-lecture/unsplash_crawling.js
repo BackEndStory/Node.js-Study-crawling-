0ebe49cc1cfd6e65bc9f0273ede03a0c6810a0ad
seungryeol <opop0421@naver.com>
2023-03-29 20:02:12 +0900

    unsplash사이트 무한 스크롤 이미지 퍼오기

--- crawling-lecture/unsplash_crawling.js
+++ crawling-lecture/unsplash_crawling.js
@@ -2,68 +2,99 @@ const puppeteer = require('puppeteer');
 const axios = require('axios');
 const fs = require('fs');
 
-
-// const splash_crawler = async () => {
-//     try {
-
-//         const browser = await puppeteer.launch({ headless: true });
-//         const page = await browser.newPage();
-//         await page.goto('https://unsplash.com');
-//         const result = await page.evaluate(() => {
-//             img_list = [];
-//             const img = document.querySelectorAll('div.ripi6 img.tB6UZ.a5VGX');
-//             if(img.length){
-//                 img.forEach((v)=>{
-//                     if(v.src){
-//                           img_list.push(v.src);
-//                     }
-//                 });
-//             }
-//             return img_list
-//         });
-//     console.log(result);
-//     } catch (e) {
-//         console.log(e);
-//     }
-
-// }
-
-// splash_crawler();
-
-
-
-const school_crawler = async () => {
+fs.readdir('imgs', (err) => {
+    if (err) {
+        console.log('imgs 폴더가 없어 imgs 폴더를 생성합니다.');
+        fs.mkdirSync('imgs');
+    }
+});
+const splash_crawler = async () => {
     try {
 
-        const browser = await puppeteer.launch({ headless: true });
+        const browser = await puppeteer.launch({ headless: true});
         const page = await browser.newPage();
-        await page.goto('https://www.shinhan.ac.kr/kr/194/subview.do');
-        const result = await page.evaluate(() => {
-            name_list = [];
-            url_list = [];
-            const name = document.querySelectorAll('a.artclLinkView strong');
-            if (name.length) {
-                name.forEach((v) => {
+        await page.goto('https://unsplash.com');
+        result = [];
+        while (result.length <= 50) {
 
-                    name_list.push(v.textContent);
+            const srcs = await page.evaluate(() => {
+                const img_list = [];
+                const img = document.querySelectorAll('div.MorZF ');
 
-                });}
-            const url = document.querySelectorAll('a.artclLinkView');
-            if (url.length) {
-                url.forEach((v) => {
+                if (img.length) {
+                    img.forEach((v) => {
+                        let src = v.querySelector("img.tB6UZ.a5VGX").src;
+                        if (src) {
+                            img_list.push(src);
+                        }
+                        v.parentElement.removeChild(v);                 // 해당 태그 삭제
+                    });
+                }
+                window.scrollBy(0, 100);                                 // 스크롤 움직이기
+                setTimeout(() => {
+                    window.scrollBy(0, 200);
+                }, 500);
 
-                    url_list.push(v.href);
-            
-                });
-               }
-                return {name_list,url_list}
+                return img_list
             });
-        console.log(result.name_list);
-        console.log(result.url_list);
+            result = result.concat(srcs);
+
+
+            await page.waitForSelector('div.MorZF');                         // 태그 재설정
+            console.log("새 이미지 태그 로딩 완료");
+        }
+        console.log(result);
+        result.forEach(async (src) => {
+            const imageResult = await axios.get(src.replace(/\?.*$/, ''), {
+                responseType: 'arraybuffer'
+            });
+            fs.writeFileSync(`imgs/${new Date().valueOf()}.jpeg`, imageResult.data);
+
+        });
+        await page.close();
+        await browser.close();
     } catch (e) {
         console.log(e);
     }
 
 }
 
-school_crawler();
+splash_crawler();
+
+
+
+// const school_crawler = async () => {
+//     try {
+
+//         const browser = await puppeteer.launch({ headless: true });
+//         const page = await browser.newPage();
+//         await page.goto('https://www.shinhan.ac.kr/kr/194/subview.do');
+//         const result = await page.evaluate(() => {
+//             name_list = [];
+//             url_list = [];
+//             const name = document.querySelectorAll('a.artclLinkView strong');
+//             if (name.length) {
+//                 name.forEach((v) => {
+
+//                     name_list.push(v.textContent);
+
+//                 });}
+//             const url = document.querySelectorAll('a.artclLinkView');
+//             if (url.length) {
+//                 url.forEach((v) => {
+
+//                     url_list.push(v.href);
+            
+//                 });
+//                }
+//                 return {name_list,url_list}
+//             });
+//         console.log(result.name_list);
+//         console.log(result.url_list);
+//     } catch (e) {
+//         console.log(e);
+//     }
+
+// }
+
+// school_crawler();
