0814112dfb2a6053720423ffc418c442d68adcde
seungryeol <opop0421@naver.com>
2023-04-04 11:18:13 +0900

    개발자 도구 network 응답으로 반응

--- crawling-lecture/facebook.js
+++ crawling-lecture/facebook.js
@@ -22,18 +22,21 @@ const faceBookLogin = async () => {
         //     document.querySelector("#pass").value = "cn37rqww@";
         // },id,password);
 
-        await page.type("#email",process.env.ID);    //
-        await page.type("#pass",process.env.PASS);
+        await page.type("#email", process.env.ID);    //
+        await page.type("#pass", process.env.PASS);
         await page.waitForSelector("._6ltg");         // 해당 버튼 태그 기다리기
         await page.hover("._6ltg");
         await page.waitForTimeout(2000);
         await page.click("._6ltg");                   // 해당 버튼 클릭 시
+        await page.waitForResponse((response)=>{
+            return response.url().includes('login_attempt');    // 단순 시간을 기다리기보다 개발자 도구 네트워크를 통해 로그인이 완료 됐다는 신호를 받고 실행
+        });
 
-       
+
         await page.waitForTimeout(6000);
         await page.keyboard.press("Escape");
         await page.click("div.x1rg5ohu.x1n2onr6.x3ajldb.x1ja2u2z");
-     
+
 
         // await page.close();
         // await browser.close();
