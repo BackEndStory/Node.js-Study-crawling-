4705d18bcaa4be7d0b9a223a3698221a283c5234
seungryeol <opop0421@naver.com>
2023-04-27 18:12:29 +0900

    정규식 풀이

--- 정규식.js
+++ 정규식.js
@@ -1,3 +1,3 @@
 const char = "aaaaadocument.write(+(TwoOneFourOne^FourFourSix)+(Two1OneFour^SixFiveZero)+(One9EightSix^One0Eight)+(ThreeSevenFiveTwo^NineEightOne))";
-const a = char.replace(/document\.write\(\+/,"");
+const a = char.replace(/\(.+/,"");   // (에 모든 문자 숫자가 오는 => . 반복되는 것들을 모두 => + 를 모두 빈 문자열로 만든다.
 console.log(a);
