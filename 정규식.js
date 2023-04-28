48c6d6731257df3f62cff1535ccd7e33107b5297
seungryeol <opop0421@naver.com>
2023-04-28 13:25:17 +0900

    정규식 분석3

--- 정규식.js
+++ 정규식.js
@@ -1,3 +1,11 @@
 const char = "aaaaadocument.write(+(TwoOneFourOne^FourFourSix)+(Two1OneFour^SixFiveZero)+(One9EightSix^One0Eight)+(ThreeSevenFiveTwo^NineEightOne))";
 const a = char.replace(/\(.+/,"");   // (에 모든 문자 숫자가 오는 => . 반복되는 것들을 모두 => + 를 모두 빈 문자열로 만든다.
-console.log(a);
+//console.log(a);
+
+
+const practice1 = "aaa .(2334aplle)+(abcdefg)";
+console.log(practice1.replace(/\.\(.+/,""));
+console.log(practice1.replace(/\s/,""));
+console.log(practice1.replace(/\d/,""));
+console.log(practice1.replace(/\D/,""));
+console.log(practice1.replace(/\b/,""));
