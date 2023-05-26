4589f8dbe956eb2abd2fb7617ff886cda5ac1b1c
seungryeol <opop0421@naver.com>
2023-05-26 16:39:56 +0900

    유튜브 베스트 인기 영상 다운로드

--- crawling-lecture/youtube.js
+++ crawling-lecture/youtube.js
@@ -1,8 +1,11 @@
 const puppeteer = require('puppeteer-extra');
-const fs = require('fs'); const dotenv = require('dotenv');
+const fs = require('fs'); 
+const dotenv = require('dotenv');
 dotenv.config({ path: "../.env" });   // dotenv사용 시 path  설정해주기
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-puppeteer.use(StealthPlugin());
+const ytdl = require('ytdl-core');
+
+
 const youtube = async () => {
     try {
 
@@ -17,18 +20,25 @@ const youtube = async () => {
             width: 1080,
             height: 1080
         });
-        await page.goto('https://youtube.com', {
+        await page.goto('https://youtube.com/feed/trending', {
             waitUntil: 'networkidle0',
         });
 
-        await page.waitForTimeout(2000);
-        await page.waitForSelector('yt-button-shape > a');
-        await page.click('yt-button-shape > a');
-        await page.waitForSelector('#identifierId');
-        await page.waitForTimeout(3000);
-        await page.type('#identifierId', process.env.GOOGLEEMAIL);
-        await page.waitForTimeout(1000);
-        await page.click("#identifierNext");                 // 크롬 자체에서 막음
+        await page.waitForSelector('ytd-video-renderer');
+        await page.click('ytd-video-renderer');
+
+        const url = await page.url();
+        const info = await ytdl.getInfo(url);
+        console.log(info);
+        ytdl(url).pipe(fs.createWriteStream( `${info.videoDetails.title}.mp4`))
+
+        // await page.waitForSelector('yt-button-shape > a');
+        // await page.click('yt-button-shape > a');
+        // await page.waitForSelector('#identifierId');
+        // await page.waitForTimeout(3000);
+        // await page.type('#identifierId', process.env.GOOGLEEMAIL);
+        // await page.waitForTimeout(1000);
+        // await page.click("#identifierNext");                 // 크롬 자체에서 막음
         // const login = await page.evaluate(() => {
         //     document.querySelector('#identifierId');
         // })
